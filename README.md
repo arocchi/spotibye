11653dbb5caf517dcb1b6207bacf1066fc5107c7
Alessio <alessio.rocchi@iit.it>
2016-08-06 14:55:24 +0200

    Refactored analysis, flattened track structure for playlists. Updated README.md

--- README.md
+++ README.md
@@ -1,5 +1,54 @@
 # spotibye
+Spotibye is a tool that allows to migrate from Spotify to Google Play Music.
+It is built for music lovers who have a big collection of Music, in particolar, it is useful for those who met the 10k Song limit in Spotify's "Your Music".
+The idea of conversion in Spotibye focuses around the *album* rather than the playlist.
+While there are already out there a number of tools to export Spotify playlists to Google Play Music, there is no easy way to transfer the whole Music Library ("Your Music").
+Furthermore, for old time users, it is typical to have a collection where albums are either saved in "Your Music" as `Albums`, or as playlists, where each playlist represents an album.
+Spotibye will therefore detect whose playlist are albums (1 playlist for 1 album scenarios), and also download a list of saved Albums in "Your Music", and save them in Google Play Music.
+It will give useful reports on the migration process, so that grey areas and failed migrations (i.e., albums which are present in one catalog and missing in the other) will be notified to the user.
+Also, in the migration process it will try to avoid duplicates, thus keeping the Google Play Music library tidy: this allows to slim down the number of tracks saved in Google Library, which is also limited to 30k at the moment, and use whenever possible the uploaded albums (for which there is a limit of 50k tracks). Theoretically, this allows to have a Music Library in Google Play Music of up to 80k tracks.
+The rationale behind all of this is:
+_a playlist is not an album_
 
+## Tools
 - analysis: get a cleaned-up list of all albums in collection
-- cleanup: from "My Music" to playlist and vice-versa
-- list of albums and automatic album search and import in google play music
+
+### Data structures:
+The data structures used are the same documented by Spotify, with the difference that `albums` and `playlist tracks` objects are "flattened", that is, the `date_added` field is moved inside the data structure:
+```
+item (paged playlist track)
+{
+    date_added: '...',
+    track: {...}
+}
+```
+becomes
+```
+track
+{
+    date_added: '...',
+    ...
+}
+```
+and the same for user albums
+```
+item (paged user album list)
+{
+    date_added: '...',
+    album: {...}
+}
+```
+becomes
+```
+album
+{
+    date_added: '...',
+    ...
+}
+```
+
+This flattening allows to use the same data structure for albums coming from playlists and albums coming from the user library, and from tracks coming from playlists and tracks coming from albums.
+
+## Planned tools
+- spotiwhy: convert from "My Music" to playlist and vice-versa, when possible (all in Spotify)
+- spotibye: list all spotify albums (either from "Your Music" or from playlists) and perform a (semi-)automatic album search and import in Google Play Music
--- analysis.py
+++ analysis.py
@@ -1,183 +1,19 @@
-# !/bin/env python
+#!/usr/bin/env python
 # coding=utf-8
 # shows a user's playlists (need to be authenticated via oauth)
 
 import sys
 import spotipy
 import spotipy.util as util
-import pickle, os
+from cachedrequest import CachedRequest
 from pprint import pprint
 
+
+
 def show_tracks(tracks):
-    for i, item in enumerate(tracks['items']):
-        track = item['track']
+    for i, track in enumerate(tracks):
         print "   %d %32.32s %s" % (i, track['artists'][0]['name'],
             track['name'])
-
-class CachedRequest(object):
-    def __init__(self, sp):
-        self.sp = sp
-        if os.path.isfile('./tracks.cache'):
-            self.tracks_dict = pickle.load(open('tracks.cache', 'r'))
-        else:
-            self.tracks_dict = dict()
-
-        if os.path.isfile('./playlists_tracks.cache'):
-            self.playlists_tracks_dict = pickle.load(open('playlists_tracks.cache', 'r'))
-        else:
-            self.playlists_tracks_dict = dict()
-
-        if os.path.isfile('./albums.cache'):
-            self.albums_dict = pickle.load(open('albums.cache', 'r'))
-        else:
-            self.albums_dict = dict()
-
-    def get_playlist_tracks(self, playlist):
-        if self.playlists_tracks_dict.has_key(playlist['id']):
-            return self.playlists_tracks_dict[playlist['id']]
-
-        tracks = []
-        results = self.sp.user_playlist(playlist['owner']['id'], playlist['id'], fields="tracks,next")
-        i_tracks = results['tracks']
-        tracks += i_tracks['items']
-        while i_tracks['next']:
-            i_tracks = self.sp.next(i_tracks)
-            tracks += i_tracks['items']
-
-        if playlist['tracks']['total'] != len(tracks):
-            raise Exception('Read the wrong number of tracks: %d of %d'%
-                            (len(tracks), playlist['tracks']['total']) )
-
-        self.playlists_tracks_dict[playlist['id']] = tracks
-        pickle.dump(self.playlists_tracks_dict, open('playlists_tracks.cache', 'wb'))
-
-        return tracks
-
-    def get_albums_tracks(self, album):
-        if self.tracks_dict.has_key(album['id']):
-            return self.tracks_dict[album['id']]
-
-        tracks = []
-        results = self.sp.user_playlist(playlist['owner']['id'], playlist['id'], fields="tracks,next")
-        i_tracks = results['tracks']
-        tracks += i_tracks['items']
-        while i_tracks['next']:
-            i_tracks = self.sp.next(i_tracks)
-            tracks += i_tracks['items']
-
-        if playlist['tracks']['total'] != len(tracks):
-            raise Exception('Read the wrong number of tracks: %d of %d'%
-                            (len(tracks), playlist['tracks']['total']) )
-
-        self.tracks_dict[playlist['id']] = tracks
-        pickle.dump(self.tracks_dict, open('tracks.cache', 'wb'))
-
-        return tracks
-
-    def get_playlists(self, exclude = None, username = None):
-        if os.path.isfile('./playlists.cache'):
-            items = pickle.load(open('playlists.cache', 'r'))
-            items = [playlist for playlist in items if
-                     playlist['name'] not in exclude and (username is None or playlist['owner']['id'] == username)]
-            return items
-
-        n_total = None
-        items = []
-        playlists = self.sp.user_playlists(username)
-        n_total = playlists['total']
-        items += playlists['items']
-        while playlists['next']:
-            playlists = self.sp.next(playlists)
-            items += playlists['items']
-
-        if len(items) != n_total:
-            raise Exception('Read the wrong number of playlists')
-
-        pickle.dump(items, open('playlists.cache', 'wb'))
-        items = [playlist for playlist in items if playlist['name'] not in exclude and ( username is None or playlist['owner']['id'] == username ) ]
-        return items
-
-    def get_your_music_albums(self):
-        if os.path.isfile('./yourmusic.cache'):
-            items = pickle.load(open('yourmusic.cache', 'r'))
-            dump_pickle = False
-            for item in items:
-                if not self.albums_dict.has_key(item['id']):
-                    dump_pickle = True
-                    self.albums_dict[item['id']] = item
-            if dump_pickle:
-                pickle.dump(self.albums_dict, open('albums.cache', 'wb'))
-            return items
-
-        n_total = None
-        items = []
-        albums = self.sp.current_user_saved_albums()
-        n_total = albums['total']
-        for item in albums['items']:
-            item['album']['added_at'] = item['added_at']
-        items += [item['album'] for item in albums['items']]
-        while albums['next']:
-            albums = self.sp.next(albums)
-            for item in albums['items']:
-                item['album']['added_at'] = item['added_at']
-            items += [item['album'] for item in albums['items']]
-
-        if len(items) != n_total:
-            raise Exception('Read the wrong number of albums')
-
-        pickle.dump(items, open('yourmusic.cache', 'wb'))
-        for item in items:
-            print item
-            if not self.albums_dict.has_key(item['id']):
-                dump_pickle = True
-                self.albums_dict[item['id']] = item
-        if dump_pickle:
-            pickle.dump(self.albums_dict, open('albums.cache', 'wb'))
-        return items
-
-    def get_playlists_albums(self, playlists):
-        albums = len(playlists)*[None]
-        albums_id = len(playlists)*[None]
-
-        for i, playlist in enumerate(playlists):
-            tracks = self.get_playlist_tracks(playlist)
-            playlist_album = tracks[0]['track']['album']
-            if not all(track['track']['album']['id'] == playlist_album['id'] for track in tracks):
-                continue
-                #raise Exception('Error: %s containts tracks from multiple albums'%playlist['name'])
-            albums_id[i] = playlist_album['id']
-
-            if self.albums_dict.has_key(playlist_album['id']):
-                albums[i] = self.albums_dict[playlist_album['id']]
-
-        dump_pickle = False
-        req = []
-        for i in xrange(len(albums)):
-            if albums[i] is None and albums_id[i] is not None:
-                dump_pickle = True
-
-                id = albums_id[i]
-
-                req.append({'i':i, 'id':id})
-
-                if len(req) == 20 or i == len(albums)-1:
-                    res = sp.albums([r['id'] for r in req])
-                    for j, album in enumerate(res['albums']):
-                        self.albums_dict[req[j]['id']] = album
-                        albums[req[j]['i']] = album
-                        playlist = playlists[req[j]['i']]
-                        self.tracks_dict[album['id']] = self.playlists_tracks_dict[playlist['id']]
-                    req = []
-
-
-        if dump_pickle:
-            pickle.dump(self.tracks_dict, open('tracks.cache', 'wb'))
-            pickle.dump(self.albums_dict, open('albums.cache', 'wb'))
-
-        return albums
-
-    def get_playlist_album(self, playlist):
-        return self.get_playlists_albums([playlist])[0]
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -202,8 +38,8 @@ if __name__ == '__main__':
 
             tracks = r.get_playlist_tracks(playlist)
 
-            playlist_album = tracks[0]['track']['album']
-            if all(track['track']['album']['id'] == playlist_album['id'] for track in tracks):
+            playlist_album = tracks[0]['album']
+            if all(track['album']['id'] == playlist_album['id'] for track in tracks):
                 for separator in [u' \u2014 ', u' \u2013 ', " - ", None]:
                     if separator is None:
                         raise Exception('Error: no separator worked for %s', playlist['name'].split(u' \u2014 '))
